9a9b45845e8cf9fcc9efa99b3c92fde7d4e7e308
tediko <tediko123@wp.pl>
2020-11-09 09:34:21 +0100

    Added comments for readability

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const shareButton = document.querySelector('[data-share]');
 const shareOverlay = document.querySelector('[data-share-overlay]');
 
+/* Display overlay on screen */
 const showOverlay = () => {
     shareButton.style.pointerEvents = 'none';
     if (shareOverlay.classList.contains('active')) {
@@ -13,17 +14,21 @@ const showOverlay = () => {
         shareButton.classList.add('active');
     }
 }
-
+/* Remove overlay when the user clicks on share link inside or contaienr itself  */
 const removeOverlay = () => {
     shareOverlay.classList.remove('active');
-    shareOverlay.classList.add('close');
     shareButton.classList.remove('active');
+    shareOverlay.classList.add('close');
 }
 
+/* Disable button until the animation is complete */
+/* Prevent spamming shareButton before animation ends */
 const removeButtonCta = () => {
     shareButton.style.pointerEvents = 'auto';
 }
 
+/* Remove all active classes on shareButton/shareOverlay while window resizing */
+/* to prevent bugs with animations*/
 const removeActiveOverlay = () => {
     shareButton.style.pointerEvents = 'auto';
     shareOverlay.classList.remove('close');
@@ -31,6 +36,7 @@ const removeActiveOverlay = () => {
     shareOverlay.classList.remove('active');
 }
 
+/* Event listeners */
 shareButton.addEventListener('click', showOverlay);
 shareOverlay.addEventListener('click', removeOverlay);
 shareOverlay.addEventListener('animationend', removeButtonCta);
